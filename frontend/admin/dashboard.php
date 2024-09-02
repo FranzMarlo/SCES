0a3e55df21cd79603c547296e336fae56ae5505b
Franz Marlo Andal <andalfranzb@gmail.com>
2024-09-02 21:39:40 +0800

    improved admin and installed chart.js

--- frontend/admin/dashboard.php
+++ frontend/admin/dashboard.php
@@ -13,8 +13,74 @@ include $_SERVER['DOCUMENT_ROOT'] . '/SCES/frontend/admin/partials/admin-head.ph
     include $_SERVER['DOCUMENT_ROOT'] . '/SCES/frontend/admin/partials/admin-sidebar.php';
     ?>
     <div class="content">
-    
+      <div class="header">
+        <div class="header-item">
+          <h1>Home</h1>
+        </div>
+        <div class="header-item">
+          <i class="fa-solid fa-bell"></i>
+          <img src="/SCES/assets/images/currentuser.png" alt="admin icon">
+        </div>
+      </div>
+      <div class="welcome-box">
+        <div class="welcome-text">
+          <div class="current-time">
+            <i class="fa-solid fa-calendar"></i>
+            <span>September 01, 2024</span>
+            <span>3:46 pm</span>
+          </div>
+          <div class="text-content">
+            <h1>Good Day, Ma'am Lozano!</h1>
+            <span>Have a nice learning day!</span>
+          </div>
+        </div>
+        <div class="welcome-img">
+          <img src="/SCES/assets/images/admin-dashboard-icon.png" alt="admin dashboard icon">
+        </div>
+      </div>
+      <div class="box-container">
+        <div class="box students">
+          <div class="box-icon">
+            <img src="/SCES/assets/images/total-students-icon.png" alt="total students icon">
+          </div>
+          <div class="box-text">
+            <h1>Total Students</h1>
+            <span>100</span>
+          </div>
+        </div>
+        <div class="box teachers">
+          <div class="box-icon">
+            <img src="/SCES/assets/images/total-teachers-icon.png" alt="total teachers icon">
+          </div>
+          <div class="box-text">
+            <h1>Total Teachers</h1>
+            <span>10</span>
+          </div>
+        </div>
+        <div class="box subjects">
+          <div class="box-icon">
+            <img src="/SCES/assets/images/total-subjects-icon.png" alt="total subjects icon">
+          </div>
+          <div class="box-text">
+            <h1>Total Subjects</h1>
+            <span>8</span>
+          </div>
+        </div>
+        <div class="box quizzes">
+          <div class="box-icon">
+            <img src="/SCES/assets/images/total-quizzes-icon.png" alt="total quizzes icon">
+          </div>
+          <div class="box-text">
+            <h1>Total Quizzes</h1>
+            <span>12</span>
+          </div>
+        </div>
+      </div>
+      <div class="chart-container">
+        
+      </div>
     </div>
+
   </div>
   <?php
   include $_SERVER['DOCUMENT_ROOT'] . '/SCES/frontend/admin/partials/admin-footer.php';
