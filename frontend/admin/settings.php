6152d21e8db9f04956b11ff321858e81bdee18ef
Franz Marlo Andal <andalfranzb@gmail.com>
2024-09-06 23:33:04 +0800

    modified admin settings and help

--- frontend/admin/settings.php
+++ frontend/admin/settings.php
@@ -2,7 +2,7 @@
 include $_SERVER['DOCUMENT_ROOT'] . '/SCES/frontend/admin/partials/admin-head.php';
 $page = '';
 ?>
-<link rel="stylesheet" href="/SCES/assets/style/admin-quizzes.css" />
+<link rel="stylesheet" href="/SCES/assets/style/settings.css" />
 <title>Quizzes | SCES Online Learning Platform</title>
 </head>
 
@@ -18,8 +18,170 @@ $page = '';
             <?php
             include $_SERVER['DOCUMENT_ROOT'] . '/SCES/frontend/admin/partials/admin-header.php';
             ?>
-            
+            <div class="title-box">
+                <h1>Account Settings</h1>
+            </div>
+            <div class="settings-container">
+                <div class="side-panel">
+                    <button class="panel-item active" onclick="toggleMyProfile()">My Profile</button>
+                    <button class="panel-item" onclick="toggleSecurity()">Security</button>
+                    <button class="panel-item">Notifications</button>
+                </div>
+                <div class="main-panel" id="myProfile">
+                    <div class="panel-title">
+                        <h1>My Profile</h1>
+                    </div>
+                    <div class="user-profile">
+                        <div class="profile-part">
+                            <div class="user-icon">
+                                <img src="/SCES/student-storage/images/<?php echo $image; ?>" alt="user icon"
+                                    id="current-user-avatar">
+                                <img src="/SCES/assets/images/change-avatar.png" alt="user icon"
+                                    id="change-avatar-icon">
+                            </div>
+                            <div class="profile-info">
+                                <h1><?php echo htmlspecialchars($studentFname . ' ' . $studentLname); ?></h1>
+                                <span><?php echo htmlspecialchars($gradeLevel . ' - ' . $section); ?></span>
+                                <span>Student ID: <?php echo htmlspecialchars($studentId); ?></span>
+                            </div>
+                        </div>
+                        <div class="profile-part">
+                            <div class="button-container">
+                                <button class="edit-btn" id="edit-profile-info">Edit<i
+                                        class="fa-solid fa-pencil"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="current-user-info">
+                        <div class="current-user-info-header">
+                            <div class="current-user-header-title">
+                                <h2>Personal Information</h2>
+                            </div>
+                            <div class="info-button-container">
+                                <button class="edit-btn" id="edit-personal-info">Edit<i
+                                        class="fa-solid fa-pencil"></i></button>
+                            </div>
+                        </div>
+                        <div class="info-content">
+                            <div class="info-part">
+                                <div class="info-data">
+                                    <span>First Name</span>
+                                    <p><?php echo htmlspecialchars($studentFname); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Last Name</span>
+                                    <p><?php echo htmlspecialchars($studentLname); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Middle Name</span>
+                                    <p><?php echo htmlspecialchars($studentMname); ?></p>
+                                </div>
+                            </div>
+                            <div class="info-part">
+                                <div class="info-data">
+                                    <span>Age</span>
+                                    <p><?php echo ($age == 0) ? 'Not Set' : htmlspecialchars($age); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Gender</span>
+                                    <p><?php echo htmlspecialchars($gender); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Email Address</span>
+                                    <p><?php echo htmlspecialchars($email); ?></p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="current-user-info">
+                        <div class="current-user-info-header">
+                            <div class="current-user-header-title">
+                                <h2>Background Information</h2>
+                            </div>
+                            <div class="info-button-container">
+                                <button class="edit-btn" id="edit-background-info">Edit<i
+                                        class="fa-solid fa-pencil"></i></button>
+                            </div>
+                        </div>
+                        <div class="info-content">
+                            <div class="info-part">
+                                <div class="info-data">
+                                    <span>City/State</span>
+                                    <p><?php echo htmlspecialchars($city); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Barangay</span>
+                                    <p><?php echo htmlspecialchars($barangay); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Street</span>
+                                    <p><?php echo htmlspecialchars($street); ?></p>
+                                </div>
+                            </div>
+                            <div class="info-part">
+                                <div class="info-data">
+                                    <span>Guardian Full Name</span>
+                                    <p><?php echo htmlspecialchars($guardianName); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Guardian Contact Number</span>
+                                    <p><?php echo htmlspecialchars($guardianContact); ?></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-panel" id="security">
+                    <div class="panel-title">
+                        <h1>Security</h1>
+                    </div>
+                    <div class="current-user-info">
+                        <div class="current-user-info-header">
+                            <div class="current-user-header-title">
+                                <h2>Password</h2>
+                            </div>
+                            <div class="info-button-container">
+                                <button class="edit-btn" id="edit-personal-info">Edit<i
+                                        class="fa-solid fa-pencil"></i></button>
+                            </div>
+                        </div>
+                        <div class="info-content">
+                            <div class="info-part">
+                                <div class="info-data">
+                                    <span>First Name</span>
+                                    <p><?php echo htmlspecialchars($studentFname); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Last Name</span>
+                                    <p><?php echo htmlspecialchars($studentLname); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Middle Name</span>
+                                    <p><?php echo htmlspecialchars($studentMname); ?></p>
+                                </div>
+                            </div>
+                            <div class="info-part">
+                                <div class="info-data">
+                                    <span>Age</span>
+                                    <p><?php echo ($age == 0) ? 'Not Set' : htmlspecialchars($age); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Gender</span>
+                                    <p><?php echo htmlspecialchars($gender); ?></p>
+                                </div>
+                                <div class="info-data">
+                                    <span>Email Address</span>
+                                    <p><?php echo htmlspecialchars($email); ?></p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
     </div>
     <?php
     include $_SERVER['DOCUMENT_ROOT'] . '/SCES/frontend/admin/partials/admin-footer.php';
