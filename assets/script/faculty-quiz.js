4c4db091994aa7a3c32f590d1fb3690438b2837e
Franz Marlo Andal <andalfranzb@gmail.com>
2024-09-29 18:48:45 +0800

    view-quiz

--- assets/script/faculty-quiz.js
+++ assets/script/faculty-quiz.js
@@ -8,66 +8,139 @@ document.addEventListener("DOMContentLoaded", function () {
   var closeModal = document.querySelector(".close-btn");
 
   if (modal && addQuizBtn && addQuizItem && closeModal) {
-      addQuizBtn.onclick = function () {
+    addQuizBtn.onclick = function () {
+      modal.style.display = "flex";
+    };
+    addQuizItem.onclick = function () {
+      modal.style.display = "flex";
+    };
+
+    var noDataItems = document.querySelectorAll(".no-data-item");
+    if (noDataItems.length) {
+      noDataItems.forEach(function (item) {
+        item.onclick = function () {
           modal.style.display = "flex";
-      };
-      addQuizItem.onclick = function () {
-          modal.style.display = "flex";
-      };
+        };
+      });
+    }
 
-      var noDataItems = document.querySelectorAll(".no-data-item");
-      if (noDataItems.length) {
-          noDataItems.forEach(function (item) {
-              item.onclick = function () {
-                  modal.style.display = "flex";
-              };
-          });
+    if (noQuizHeader) {
+      noQuizHeader.onclick = function () {
+        modal.style.display = "flex";
+      };
+    }
+
+    closeModal.onclick = function () {
+      modal.style.display = "none";
+      form.reset();
+      resetLessonDropdown();
+    };
+
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+        form.reset();
+        resetLessonDropdown();
       }
-
-      if (noQuizHeader) {
-          noQuizHeader.onclick = function () {
-              modal.style.display = "flex";
-          };
-      }
-
-      closeModal.onclick = function () {
-          modal.style.display = "none";
-          form.reset();
-          resetLessonDropdown();
-      };
-
-      window.onclick = function (event) {
-          if (event.target == modal) {
-              modal.style.display = "none";
-              form.reset();
-              resetLessonDropdown();
-          }
-      };
+    };
   }
 
   function resetLessonDropdown() {
-      lessonDropdown.innerHTML = '<option value="" selected>Select Subject First</option>';
+    lessonDropdown.innerHTML =
+      '<option value="" selected>Select Subject First</option>';
   }
 
   document.getElementById("subject").addEventListener("change", function () {
-      var selectedOption = this.options[this.selectedIndex];
+    var selectedOption = this.options[this.selectedIndex];
+    var levelId = selectedOption.getAttribute("data-level-id");
+    var subjectId = selectedOption.value;
+    var sectionId = selectedOption.getAttribute("data-section-id");
 
-      var levelId = selectedOption.getAttribute("data-level-id");
-      var subjectId = selectedOption.value;
-      var sectionId = selectedOption.getAttribute("data-section-id");
+    if (subjectId) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "/SCES/backend/faculty/fetch-lessons.php", true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-      if (subjectId) {
-          var xhr = new XMLHttpRequest();
-          xhr.open("POST", "/SCES/backend/faculty/fetch-lessons.php", true);
-          xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+          lessonDropdown.innerHTML = xhr.responseText;
+        }
+      };
 
-          xhr.onreadystatechange = function () {
-              if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                  lessonDropdown.innerHTML = xhr.responseText;
-              }
-          };
-
-          xhr.send("levelId=" + levelId + "&subjectId=" + subjectId + "&sectionId=" + sectionId);
-      }
+      xhr.send(
+        "levelId=" +
+          levelId +
+          "&subjectId=" +
+          subjectId +
+          "&sectionId=" +
+          sectionId
+      );
+    }
   });
+
+  // New Functionality: Display quiz headers and questions
+  var pendingItems = document.querySelectorAll(".pending-item");
+  var quizHeaders = document.querySelectorAll(".quiz-header");
+  var quizItems = document.querySelectorAll(".quiz-item");
+
+  // Hide all quiz headers and questions initially
+  quizHeaders.forEach(function (header) {
+    header.style.display = "none";
+  });
+  quizItems.forEach(function (item) {
+    item.style.display = "none";
+  });
+
+  // Event listeners for pending items
+  pendingItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      // Get the index of the clicked pending item
+      var index = item.getAttribute("data-quiz-index");
+
+      // Hide all quiz headers and questions
+      quizHeaders.forEach(function (header) {
+        header.style.display = "none";
+      });
+      quizItems.forEach(function (item) {
+        item.style.display = "none";
+      });
+
+      // Show the corresponding quiz header and questions
+      if (quizHeaders[index]) {
+        quizHeaders[index].style.display = "block";
+        quizItems[index].style.display = "block";
+      }
+    });
+  });
+
+  // Logic for quiz dropdown buttons
+  var quizButtons = document.querySelectorAll(
+    ".quiz-dropdown-content button[data-quiz-index]"
+  );
+
+  quizButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      var index = button.getAttribute("data-quiz-index");
+
+      // Hide all quiz headers and items
+      quizHeaders.forEach(function (header) {
+        header.style.display = "none";
+      });
+      quizItems.forEach(function (item) {
+        item.style.display = "none";
+      });
+
+      // Show the corresponding quiz header and item
+      if (quizHeaders[index]) {
+        quizHeaders[index].style.display = "block";
+        quizItems[index].style.display = "block";
+      }
+    });
+  });
+  if (quizHeaders.length > 0 && quizItems.length > 0) {
+    quizHeaders[0].style.display = "block";
+    quizItems[0].style.display = "block";
+  } else if (pendingItems.length > 0) {
+    pendingItems[0].click(); // Simulate click on the first pending item
+  }
 });
--- frontend/faculty/quizzes.php
+++ frontend/faculty/quizzes.php
@@ -40,14 +40,14 @@ $page = '';
                                 Add Quiz
                             </button>
                             <?php if ($quizzes): ?>
-                                <?php foreach ($quizzes as $quiz): ?>
+                                <?php foreach ($quizzes as $index => $quiz): ?>
                                     <?php if ($quiz['status'] == 'Active'): ?>
-                                        <button class="<?php echo strtolower($quiz['subject_code']); ?>">
+                                        <button class="<?php echo strtolower($quiz['subject_code']); ?>"
+                                            data-quiz-index="<?php echo $index; ?>">
                                             <span><?php echo htmlspecialchars($quiz['title']); ?></span>
                                         </button>
                                     <?php endif; ?>
                                 <?php endforeach; ?>
-
                             <?php else: ?>
                                 <div class="no-data-item">
                                     <img src="/SCES/assets/images/no-data-icon.png" alt="no-data-icon.png">
@@ -70,14 +70,14 @@ $page = '';
                         </div>
 
                         <?php if ($quizzes): ?>
-                            <?php foreach ($quizzes as $quiz): ?>
+                            <?php foreach ($quizzes as $index => $quiz): ?>
                                 <?php if ($quiz['status'] == 'Active'): ?>
-                                    <div class="pending-item <?php echo strtolower($quiz['subject_code']); ?>">
-                                        <span><?php echo htmlspecialchars($quiz['subject'])?> - Quiz <?php echo htmlspecialchars($quiz['quiz_number'])?></span>
+                                    <div class="pending-item" data-quiz-index="<?php echo $index; ?>">
+                                        <span><?php echo htmlspecialchars($quiz['subject']) ?> - Quiz
+                                            <?php echo htmlspecialchars($quiz['quiz_number']) ?></span>
                                     </div>
                                 <?php endif; ?>
                             <?php endforeach; ?>
-
                         <?php else: ?>
                             <div class="no-data-item">
                                 <img src="/SCES/assets/images/no-data-icon.png" alt="no-data-icon.png">
@@ -89,8 +89,8 @@ $page = '';
 
                     <div class="header-container">
                         <?php if ($quizzes): ?>
-                            <?php foreach ($quizzes as $quiz): ?>
-                                <div class="quiz-header">
+                            <?php foreach ($quizzes as $index => $quiz): ?>
+                                <div class="quiz-header" data-quiz-index="<?php echo $index; ?>">
                                     <div class="header-bg <?php echo strtolower($quiz['subject_code']); ?>">
                                         <div class="icon-container">
                                             <img src="/SCES/assets/images/<?php echo htmlspecialchars($quiz['icon']); ?>"
@@ -99,27 +99,16 @@ $page = '';
                                     </div>
                                     <div class="header-text">
                                         <img src="/SCES/assets/images/quiz-1.png" alt="quiz-1.png">
-                                        <h1>Quiz <?php echo htmlspecialchars($quiz['quiz_number']) . ' - ' .htmlspecialchars($quiz['title']); ?></h1>
+                                        <h1>Quiz
+                                            <?php echo htmlspecialchars($quiz['quiz_number']) . ' - ' . htmlspecialchars($quiz['title']); ?>
+                                        </h1>
                                     </div>
                                 </div>
-
-                                <div class="quiz-item">
+                                <div class="quiz-item" data-quiz-index="<?php echo $index; ?>" style="display: none;">
                                     <?php if (!empty($quiz['questions'])): ?>
-                                        <?php foreach ($quiz['questions'] as $index => $question): ?>
+                                        <?php foreach ($quiz['questions'] as $question): ?>
                                             <div class="question-box">
-                                                <span><?php echo ($index + 1) . '. ' . htmlspecialchars($question['question_text']); ?></span>
-                                            </div>
-                                            <div class="quiz-ans">
-                                                <p>A. <?php echo htmlspecialchars($question['choice_a']); ?></p>
-                                            </div>
-                                            <div class="quiz-ans">
-                                                <p>B. <?php echo htmlspecialchars($question['choice_b']); ?></p>
-                                            </div>
-                                            <div class="quiz-ans">
-                                                <p>C. <?php echo htmlspecialchars($question['choice_c']); ?></p>
-                                            </div>
-                                            <div class="quiz-ans">
-                                                <p>D. <?php echo htmlspecialchars($question['choice_d']); ?></p>
+                                                <span><?php echo htmlspecialchars($question['question_text']); ?></span>
                                             </div>
                                         <?php endforeach; ?>
                                     <?php else: ?>
